4e82cbf986e570e6718b3b8537b9aa5154d34a08
nivedithareddy123 <swapnikaproj@gmail.com>
2019-02-05 17:49:18 +0530

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hd!@cxwe)60!e@#x9xkoggtgwmts+#8q$%u@f2e#*^k0zuxk7p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
